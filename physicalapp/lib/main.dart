7263bad5b78aa84ec9240530bc4fe14349602bf6
AAtrox <akobe200@gmail.com>
2025-06-26 18:31:54 +0800

    login

--- physicalapp/lib/main.dart
+++ physicalapp/lib/main.dart
@@ -14,6 +14,7 @@ import 'package:flutter/services.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+  await getAnalysis(101);
 
   runApp(const MyApp());
 }
